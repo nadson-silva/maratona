335604eed91726f3b4d9ae7723c12a113530e9ad
Nadson Silva <57771786+nadson-silva@users.noreply.github.com>
2023-04-12 21:56:51 -0300

    Update balbalbal3.py

--- balbalbal3.py
+++ balbalbal3.py
@@ -1,34 +1,81 @@
-palavra_e = input('Digite a palavra:\n')
-chave_e = input('Digite os numeros para a chave:\n')
+char = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+        'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-if (len(palavra_e) % 7) == 0:
-    colunas = [""]*7
-    linhas = len(palavra_e) // 7
-    matriz = [ colunas ]*(linhas)
-else:
-    colunas = [""]*7
-    linhas = len(palavra_e) // 7 + 1
-    matriz = [ colunas ]*(linhas)
+text = input('texto').upper()
+key = input('key')
 
-cont_l = 0
-matriz_tuple = [""]*linhas
-lista_chave = [0]*len(chave_e)
-cont = -1
+def textValidation(text, key):
+    for letter in text:
+        if letter not in char:
+            return False
+    for letter in key:
+        try:
+            int(letter)
+        except:
+            return False
+    if not len(set(key)) == 7:
+        return False
+    return True 
 
-pular_prox = 0
 
-for i in chave_e:
-    cont += 1
-    lista_chave[cont] = i
 
-for cont_linha in range(0, linhas):
-    for cont_coluna in range(0, 7) :
-        index = cont_coluna + pular_prox
-        matriz[cont_linha][cont_coluna] = palavra_e[index]
-        if (cont_coluna == 6):
-            cont_l += 1
-            pular_prox += 7
-        matriz_tuple[cont_linha] = tuple(matriz[cont_linha])
+def encode(text, key):
+    if textValidation(text, key):
+        if (len(text) % 7) == 0:
+            colunas = [""]*7
+            linhas = len(text) // 7
+            matriz = [ colunas ]*(linhas)
+        else:
+            colunas = [""]*7
+            linhas = len(text) // 7 + 1
+            matriz = [ colunas ]*(linhas)
 
-print(matriz_tuple)
+        print(matriz)
+        cont_l = 0
+        matriz_tuple = [""]*linhas
+        lista_chave = [0]*len(key)
+        cont = -1
 
+        pular_prox = 0
+
+        for i in key:
+            cont += 1
+            lista_chave[cont] = i
+
+        for cont_linha in range(0, linhas):
+            for cont_coluna in range(0, 7) :
+                index = cont_coluna + pular_prox
+                try:
+                    matriz[cont_linha][cont_coluna] = text[index]
+                except:
+                    matriz[cont_linha][cont_coluna] = None
+                if (cont_coluna == 6):
+                    cont_l += 1
+                    pular_prox += 7
+                matriz_tuple[cont_linha] = tuple(matriz[cont_linha])
+
+        print(matriz_tuple)
+    
+        text_encoded = ''
+        key_array = []
+        key_array_int = []
+        
+        for i in key:
+            key_array.append(i)
+            key_array_int.append(i)
+            
+        key_array_int.sort()
+        print(key_array)
+        
+        for j in key_array_int:
+            pos = key_array.index(str(j))
+            for i in range(len(matriz_tuple)):
+                if matriz_tuple[i][pos] is not None:
+                    text_encoded += matriz_tuple[i][pos]
+
+        print(text_encoded)
+
+
+def decode():
+    
+encode(text, key)
